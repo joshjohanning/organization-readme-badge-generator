7d109fa951d96fb98eb121883c0ba493b5d82cdd
Josh Johanning <joshjohanning@github.com>
2024-06-28 16:18:39 -0500

    fix: add error handling

--- src/index.mjs
+++ src/index.mjs
@@ -164,56 +164,61 @@ const getPullRequestsCount = async (org, repo, prFilterDate) => {
 };
 
 const generateBadges = async () => {
-  // repo count
-  const repos = await getRepositories(organization);
-  const repoCount = await getRepositoryCount(organization);
-  core.info(`Total repositories: ${repoCount}`);
+  try {
+    // repo count
+    const repos = await getRepositories(organization);
+    const repoCount = await getRepositoryCount(organization);
+    core.info(`Total repositories: ${repoCount}`);
 
-  // pull requests
-  let totalOpenPRs = 0;
-  let totalMergedPRs = 0;
+    // pull requests
+    let totalOpenPRs = 0;
+    let totalMergedPRs = 0;
 
-  const date = new Date();
-  date.setUTCDate(date.getUTCDate() - days);
-  const prFilterDate = date.toISOString();
-  core.debug(`Filtering PRs created after ${prFilterDate}`);
+    const date = new Date();
+    date.setUTCDate(date.getUTCDate() - days);
+    const prFilterDate = date.toISOString();
+    core.debug(`Filtering PRs created after ${prFilterDate}`);
 
-  for (const repo of repos) {
-    const { total, merged } = await getPullRequestsCount(
-      organization,
-      repo,
-      prFilterDate,
+    for (const repo of repos) {
+      const { total, merged } = await getPullRequestsCount(
+        organization,
+        repo,
+        prFilterDate,
+      );
+      totalOpenPRs += total;
+      totalMergedPRs += merged;
+    }
+
+    core.info(
+      `Total open pull requests in last ${days} days for ${organization}: ${totalOpenPRs}`,
     );
-    totalOpenPRs += total;
-    totalMergedPRs += merged;
+    core.info(
+      `Total merged pull requests in last ${days} days for ${organization}: ${totalMergedPRs}`,
+    );
+
+    badgeMarkdown.push(
+      generateBadgeMarkdown(`Total repositories`, repoCount, "blue"),
+    );
+    badgeMarkdown.push(
+      generateBadgeMarkdown(
+        `Open PRs in last ${days} days`,
+        totalOpenPRs,
+        "blue",
+      ),
+    );
+    badgeMarkdown.push(
+      generateBadgeMarkdown(
+        `Merged PRs in last ${days} days`,
+        totalMergedPRs,
+        "blue",
+      ),
+    );
+
+    return badgeMarkdown;
+  } catch (error) {
+    core.error(error.stack);
+    process.exit(1);
   }
-
-  core.info(
-    `Total open pull requests in last ${days} days for ${organization}: ${totalOpenPRs}`,
-  );
-  core.info(
-    `Total merged pull requests in last ${days} days for ${organization}: ${totalMergedPRs}`,
-  );
-
-  badgeMarkdown.push(
-    generateBadgeMarkdown(`Total repositories`, repoCount, "blue"),
-  );
-  badgeMarkdown.push(
-    generateBadgeMarkdown(
-      `Open PRs in last ${days} days`,
-      totalOpenPRs,
-      "blue",
-    ),
-  );
-  badgeMarkdown.push(
-    generateBadgeMarkdown(
-      `Merged PRs in last ${days} days`,
-      totalMergedPRs,
-      "blue",
-    ),
-  );
-
-  return badgeMarkdown;
 };
 
 generateBadges()
